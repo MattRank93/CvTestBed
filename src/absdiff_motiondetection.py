2696f8201ab2f1de75fd496ac093b25698d02dc7
mattrank93 <ChloeRdoe1Bike2021>
2021-08-27 16:11:21 -0500

    Added alert to server via rest call.

--- src/absdiff_motiondetection.py
+++ src/absdiff_motiondetection.py
@@ -1,4 +1,6 @@
+import string
 from datetime import date
+import datetime
 import cv2
 import requests
 
@@ -22,7 +24,8 @@ def AbsDiffMd(cap, sensitivity):
         if value > thresh:
          print("Motion detected")
          # defining a params dict for the parameters to be sent to the API
-         PARAMS = {'date': date.today()}
+         now = datetime.datetime.now()
+         PARAMS = {'date': now.strftime("%Y-%m-%d %H:%M:%S")}
          # sending get request and saving the response as response object
          r = requests.post(url=URL, params=PARAMS)
          print(r.status_code)
@@ -36,6 +39,9 @@ def AbsDiffMd(cap, sensitivity):
 
         ret, frame = cap.read()
 
+        base = 10000000
+
+
 
 
 
@@ -43,7 +49,7 @@ def AbsDiffMd(cap, sensitivity):
             # use previous frame here
             prev_gray = cv2.cvtColor(previousFrame, cv2.COLOR_BGR2GRAY)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            thresh = (gray.sum() * sensitivity)
+            thresh = (base * sensitivity)
             apdresult = ADP(gray, prev_gray, thresh)
             cv2.imshow("apdresult", apdresult)
             if cv2.waitKey(100) & 0xFF == ord('q'):
